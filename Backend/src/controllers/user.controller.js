e5df237240ad485c009875d2f683c15bd1c27d5c
Hatim Malak <hatim05042006@gmail.com>
2026-09-12 13:50:25 +0530

    i have created generateTokens function to create accessToken and referesh token

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,3 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const generateTokens = (user) => {
+    const accessToken = jwt.sign(
+        { id: user.id, username: user.username, role: user.role },
+        process.env.JWT_SECRET || 'secret',
+        { expiresIn: '15m' }
+    );
+    const refreshToken = jwt.sign(
+        { id: user.id },
+        process.env.JWT_REFRESH_SECRET || 'flsahfhghlerofkhrowowoeyr',
+        { expiresIn: '7d' }
+    );
+    return { accessToken, refreshToken };
+};
+
 export const login = async(req,res) => {
     
 }
