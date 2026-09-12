36951f8bc1559cb0d972aebaf9afdfd001dd739a
Hatim Malak <hatim05042006@gmail.com>
2026-09-12 13:56:25 +0530

    created createuser controller for signup

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -30,5 +30,23 @@ export const getAllUsers = async(req,res) => {
 
 }
 export const createUser = async(req,res) => {
+    try {
+        const { username, password, role } = req.body;
 
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const result = await pool.query(
+            "INSERT INTO users (username, password_hash, role) VALUES ($1, $2, $3) RETURNING id, username, role, created_at",
+            [username, passwordHash, role]
+        );
+
+        res.status(201).json({ user: result.rows[0]});
+    } catch (err) {
+        if (err.code === '23505') {
+            return res.status(409).json({ error: "Username already exists" });
+        }
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
