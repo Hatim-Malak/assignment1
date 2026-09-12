36d5d658704980068fe85564ce3f8706576b25e9
Hatim Malak <hatim05042006@gmail.com>
2026-09-12 20:10:26 +0530

    created the getcurrent user controller so that we can get current fresh user from the database

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -123,6 +123,16 @@ export const logout = async(req,res) => {
     }
 }
 export const getCurrentUser = async(req,res) => {
+    try {
+        const result = await pool.query("SELECT id,username, role, created_at FROM users WHERE id = $1", [req.user.id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.json({ user: result.rows[0] });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
 
 }
 export const getAllUsers = async(req,res) => {
